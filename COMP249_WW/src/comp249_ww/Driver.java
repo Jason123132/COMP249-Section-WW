bf43499471746806ca752af3c5901002c7cc5f4b
abdel-concordia <abdel@MSSPro.wireless.concordia.ca>
2024-01-26 20:17:51 -0500

    Pass by value / Pass by reference simple example

--- COMP249_WW/src/comp249_ww/Driver.java
+++ COMP249_WW/src/comp249_ww/Driver.java
@@ -9,9 +9,22 @@ public class Driver {
         Person p3 = new Person("John", 19, "Montreal");
 
         System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+        someMethod(p1);
+        System.out.println(p1);
 
+        int a = 10;
+        f(a);
+        System.out.println(a);
+
+    }
+
+    public static void someMethod(Person p) {
+        //p.setName("Ali");
+        p = new Person("Justin", 50, "Ottawa");
+    }
+
+    public static void f(int x) {
+        x = x * 2;
     }
 
 }
