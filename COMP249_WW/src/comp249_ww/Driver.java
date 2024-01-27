097befbafd4f9590d7b38f30a4219db32c6d9483
abdel-concordia <abdel@MSSPro.wireless.concordia.ca>
2024-01-26 20:10:39 -0500

    Class Person and driver

--- COMP249_WW/src/comp249_ww/Driver.java
+++ COMP249_WW/src/comp249_ww/Driver.java
@@ -3,7 +3,15 @@ package comp249_ww;
 public class Driver {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Person p1 = new Person();
+        Person p2 = new Person("sdfsd", 7);
+        Person p3 = new Person("John", 19, "Montreal");
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+
     }
 
 }
