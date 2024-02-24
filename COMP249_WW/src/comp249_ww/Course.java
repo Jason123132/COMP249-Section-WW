de7e8e69901067d1b060d02abfbd5af52a306c79
abdel-concordia <abdel@MSSPro.wireless.concordia.ca>
2024-02-23 20:11:39 -0500

    Second example with Exception handling

---
+++ COMP249_WW/src/comp249_ww/Course.java
@@ -0,0 +1,43 @@
+package comp249_ww;
+
+public class Course {
+
+    private String title; // The title should never be empty
+    private double creditPoints; // credit popints should be in [0, 4]
+
+    public Course(String title, double creditPoints) throws Exception, BadCreditPointsException {
+        if (title.equals("")) {
+            throw new Exception("No empty titles please.");
+        } else {
+            this.title = title;
+        }
+        if (creditPoints < 0 || creditPoints > 4) {
+            throw new BadCreditPointsException("Bad credit points.", 0, 4);
+        } else {
+            this.creditPoints = creditPoints;
+        }
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getCreditPoints() {
+        return creditPoints;
+    }
+
+    public void setCreditPoints(double creditPoints) {
+        this.creditPoints = creditPoints;
+    }
+
+    @Override
+    public String toString() {
+        return "Course " + this.title + " and CP " + this.creditPoints;
+    }
+
+}
