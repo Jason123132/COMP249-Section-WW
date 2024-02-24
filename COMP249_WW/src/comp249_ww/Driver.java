a9b10c00af873152af4cf3f164134be95e3baf52
abdel-concordia <abdel@MSSPro.wireless.concordia.ca>
2024-02-23 19:23:48 -0500

    First example with Exception handling

--- COMP249_WW/src/comp249_ww/Driver.java
+++ COMP249_WW/src/comp249_ww/Driver.java
@@ -1,24 +1,52 @@
 package comp249_ww;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Driver {
 
     public static void main(String[] args) {
 
-        final int ARRAY_SIZE = 3;
-        Shape[] myShapes = new Shape[ARRAY_SIZE];
+        int x = 3;
 
-        myShapes[0] = new Triangle("T1", 3, 7, 5);
-        myShapes[1] = new Circle("C1", 2.5);
-        myShapes[2] = new Rectangle("R1", 5, 9);
+        int z = 0;
 
-        for (int i = 0; i < ARRAY_SIZE; i++) {
-            System.out.println(myShapes[i].getPerimeter()); // Dynamic binding
+        try {
+            int y = getNumber();
+            int b = getNumber();
+            int c = getNumber();
+            int d = getNumber();
+            z = x / (x - y); // Division by Zero
+            System.out.println("Another line of code");
+            System.out.println("Another line of code");
+            System.out.println("Another line of code");
+            System.out.println("Another line of code");
+            System.out.println("Another line of code");
+            System.out.println("Another line of code");
+            System.out.println("Another line of code");
+            System.out.println("Another line of code");
+
+        } catch (ArithmeticException ae) {
+            System.out.println("Division by zero.");
+        } catch (InputMismatchException ime) {
+            System.out.println("The input must be an int.");
+        } catch (Exception ex) {
+            System.out.println("There was a problem with your program." + ex.getMessage());
+            System.exit(0);
+        } finally {
+            System.out.println("This is finally.");
         }
 
-        // Enhanced loop (foreach loop)
-        for (Shape s : myShapes) {
-            System.out.println(s.getPerimeter());
-        }
+        System.out.println("The result is " + z);
 
     }
+
+    public static int getNumber() throws InputMismatchException {
+        System.out.print("Please input a number: ");
+        Scanner sc = new Scanner(System.in);
+
+        int y = sc.nextInt(); // InputMismatch
+        return y;
+    }
+
 }
