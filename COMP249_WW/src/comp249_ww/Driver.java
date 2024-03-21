b8145f7584ed39e406e292debb6371a70638ab93
abdel-concordia <abdel@MSSPro.wireless.concordia.ca>
2024-03-21 19:52:06 -0400

    Cloneable, Comparable example

--- COMP249_WW/src/comp249_ww/Driver.java
+++ COMP249_WW/src/comp249_ww/Driver.java
@@ -1,27 +1,71 @@
 package comp249_ww;
 
-import java.awt.SecondaryLoop;
-
 public class Driver {
 
     public static void main(String[] args) {
 
-        OurFirstInterface obj = new SomeClass();
-        OurFirstInterface obj2 = new AnotherClass();
+        A a = new A(10);
+        A c = new A(70);
 
-        OurFirstInterface objects[] = new OurFirstInterface[5];
-
-        objects[0] = obj;
-        objects[1] = obj2;
-
-        for (OurFirstInterface o : objects) {
-            if (o != null) {
-                System.out.println(o.method1(7));
-            }
+        try {
+            A b = (A) a.clone();
+        } catch (CloneNotSupportedException cnsex) {
+            System.out.println("Clone not supported.");
         }
+
+        System.out.println(a.compareTo(new B()));
     }
 
-    public void abc(OurFirstInterface obj) {
+}
 
+class A implements Cloneable, Comparable {
+
+    private B b; // B is mutable
+    private C c; // C is mutable
+    private int number; // int is primitive type
+
+    public A(int number) {
+        b = new B();
+        c = new C();
+        this.number = number;
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        A copy = (A) super.clone();
+        copy.b = (B) b.clone();
+        copy.c = (C) c.clone();
+        copy.number = number;
+
+        return copy;
+    }
+
+    @Override
+    public int compareTo(Object otherObject) throws ClassCastException {
+
+        A other = (A) otherObject;
+
+        if (number < other.number) {
+            return -1;
+        } else if (number == other.number) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
+
+class B implements Cloneable {
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
+
+class C implements Cloneable {
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 }
